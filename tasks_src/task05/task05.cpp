fa664a873ec199971a35c8c3c0586bdbccab21c9
Tizian Everke <t.everke@gmail.com>
2026-05-04 17:55:12 +0200

    Implement 5.1.0 with default cube and toggleable normals

--- tasks_src/task05/task05.cpp
+++ tasks_src/task05/task05.cpp
@@ -20,6 +20,67 @@
 #include "../ramen/rgl_model.h"
 #include "../ramen/rgl_shader.h"
 
+std::vector<Vertex> CreateCube(const Vec3f& color);
+std::vector<Vertex> CreateNormalLines(const std::vector<Vertex>& vertices, float lineLength = 0.1f, bool drawNormals = true);
+
+std::vector<Vertex> CreateCube(const Vec3f& color) {
+    std::vector<Vertex> vertices;
+    const Vec3f positions[8] = {
+        {-0.5f, -0.5f, -0.5f},
+        {0.5f, -0.5f, -0.5f},
+        {0.5f, 0.5f, -0.5f},
+        {-0.5f, 0.5f, -0.5f},
+        {-0.5f, -0.5f, 0.5f},
+        {0.5f, -0.5f, 0.5f},
+        {0.5f, 0.5f, 0.5f},
+        {-0.5f, 0.5f, 0.5f}
+    };
+    const Vec3f normals[6] = {
+        {0, 0, -1}, // front
+        {0, 0, 1},  // back
+        {-1, 0, 0}, // left
+        {1, 0, 0},  // right
+        {0, -1, 0}, // bottom
+        {0, 1, 0}   // top
+    };
+    const int indices[36] = {
+        // front
+        0, 1, 2, 0, 2, 3,
+        // back
+        5, 4, 7, 5, 7, 6,
+        // left
+        4, 0, 3, 4, 3, 7,
+        // right
+        1, 5, 6, 1, 6, 2,
+        // bottom
+        4, 5, 1, 4, 1, 0,
+        // top
+        3, 2, 6, 3, 6, 7
+    };
+    for (int i = 0; i < 36; i++) {
+        Vertex v;
+        v.position = positions[indices[i]];
+        const int face = i / 6;
+        v.normal = normals[face];
+        v.color = color;
+        vertices.push_back(v);
+    }
+    return vertices;
+}
+
+std::vector<Vertex> CreateNormalLines(const std::vector<Vertex>& vertices, float lineLength, bool drawNormals) {
+    std::vector<Vertex> lines;
+    if (!drawNormals) return lines;
+
+    for (const auto& v : vertices) {
+        Vec3f start = v.position;
+        Vec3f end = v.position + v.normal * lineLength;
+        lines.push_back({start, {0, 0, 0}, {1, 1, 1}});
+        lines.push_back({end, {0, 0, 0}, {1, 1, 1}});
+    }
+    return lines;
+}
+
 int main(int argc, char** argv)
 {
     Filesystem* pFS = Filesystem::Init(argc, argv);
@@ -41,8 +102,42 @@ int main(int argc, char** argv)
     /* Model mat*/
     Mat4f modelMat = Mat4f::Identity();
 
-    GLuint VAO;
-    glCreateVertexArrays(1, &VAO);
+    /* show normal vectors */
+    bool showNormals = false;
+
+    /* Helper function to create VBO and VAO from vertices */
+    auto CreateGeometryBuffers = [](const std::vector<Vertex>& vertices) {
+        GLuint VBO;
+        glCreateBuffers(1, &VBO);
+        glNamedBufferData(VBO, vertices.size() * sizeof(Vertex), vertices.data(), GL_STATIC_DRAW);
+
+        /* VAO. */
+        GLuint VAO;
+        glCreateVertexArrays(1, &VAO);
+        glVertexArrayVertexBuffer(VAO, 0, VBO, 0, sizeof(Vertex));
+        /* Position */
+        glVertexArrayAttribFormat(VAO, 0, 3, GL_FLOAT, GL_FALSE, 0);
+        glEnableVertexArrayAttrib(VAO, 0);
+        glVertexArrayAttribBinding(VAO, 0, 0);
+        /* Normal */
+        glVertexArrayAttribFormat(VAO, 1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float));
+        glEnableVertexArrayAttrib(VAO, 1);
+        glVertexArrayAttribBinding(VAO, 1, 0);
+        /* Color */
+        glVertexArrayAttribFormat(VAO, 2, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float));
+        glEnableVertexArrayAttrib(VAO, 2);
+        glVertexArrayAttribBinding(VAO, 2, 0);
+
+        return std::make_pair(VAO, VBO);
+    };
+
+    /* Create geometries */
+    const std::vector<Vertex> cubeVertices = CreateCube({1.0f, 0.0f, 0.0f}); // Red
+    auto [VAO_Cube, VBO_Cube] = CreateGeometryBuffers(cubeVertices);
+
+    /* Create normal lines */
+    const std::vector<Vertex> cubeNormals = CreateNormalLines(cubeVertices);
+    auto [VAO_CubeNormals, VBO_CubeNormals] = CreateGeometryBuffers(cubeNormals);
 
     /* Some global GL states */
     glEnable(GL_DEPTH_TEST);
@@ -84,7 +179,73 @@ int main(int argc, char** argv)
                     isRunning = false;
                 }
                 break;
-
+                    /* active / deactivate normals with 'N' */
+                case SDLK_N:
+                    {
+                        showNormals = !showNormals;
+                    }
+                    break;
+                    /* ADDITIONAL: Camera movement */
+                case SDLK_W:
+                    {
+                        camera.Translate(camera.GetForward() * 0.1f);
+                    }
+                    break;
+                case SDLK_S:
+                    {
+                        camera.Translate(-camera.GetForward() * 0.1f);
+                    }
+                    break;
+                case SDLK_A:
+                    {
+                        camera.Translate(-camera.GetRight() * 0.1f);
+                    }
+                    break;
+                case SDLK_D:
+                    {
+                        camera.Translate(camera.GetRight() * 0.1f);
+                    }
+                    break;
+                case SDLK_Q:
+                    {
+                        camera.Translate(camera.GetUp() * 0.1f);
+                    }
+                    break;
+                case SDLK_E:
+                    {
+                        camera.Translate(-camera.GetUp() * 0.1f);
+                    }
+                    break;
+                case SDLK_UP:
+                    {
+                        camera.Pitch(1.0f);
+                    }
+                    break;
+                case SDLK_DOWN:
+                    {
+                        camera.Pitch(-1.0f);
+                    }
+                    break;
+                case SDLK_LEFT:
+                    {
+                        camera.Yaw(1.0f);
+                    }
+                    break;
+                case SDLK_RIGHT:
+                    {
+                        camera.Yaw(-1.0f);
+                    }
+                    break;
+                case SDLK_PAGEUP:
+                    {
+                        camera.Roll(1.0f);
+                    }
+                    break;
+                case SDLK_PAGEDOWN:
+                    {
+                        camera.Roll(-1.0f);
+                    }
+                    break;
                 default:
                 {
                 }
@@ -126,10 +287,21 @@ int main(int argc, char** argv)
         glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
 
         shader.Use();
-        glBindVertexArray(VAO);
+
+        glBindVertexArray(VAO_Cube);
         glUniformMatrix4fv(0, 1, GL_FALSE, modelMat.Data());
         glUniformMatrix4fv(1, 1, GL_FALSE, viewMat.Data());
         glUniformMatrix4fv(2, 1, GL_FALSE, projMat.Data());
+        glDrawArrays(GL_TRIANGLES, 0, (GLsizei)cubeVertices.size());
+
+        if (showNormals)
+        {
+            glBindVertexArray(VAO_CubeNormals);
+            glUniformMatrix4fv(0, 1, GL_FALSE, modelMat.Data());
+            glUniformMatrix4fv(1, 1, GL_FALSE, viewMat.Data());
+            glUniformMatrix4fv(2, 1, GL_FALSE, projMat.Data());
+            glDrawArrays(GL_LINES, 0, (GLsizei)cubeNormals.size());
+        }
 
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
@@ -140,7 +312,10 @@ int main(int argc, char** argv)
 
     /* GL Resources shutdown. */
     shader.Delete();
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO_Cube);
+    glDeleteVertexArrays(1, &VAO_Cube);
+    glDeleteBuffers(1, &VBO_CubeNormals);
+    glDeleteVertexArrays(1, &VAO_CubeNormals);
 
     /* Ramen Shutdown */
     pRamen->Shutdown();
